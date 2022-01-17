c1f2a76214af1c28c611d9e7dbe93927cb67b285
amizuka000 <62437288+amizuka000@users.noreply.github.com>
2022-01-17 03:39:42 -0500

    Updated art

--- js/website.js
+++ js/website.js
@@ -1,3 +1,14 @@
+/*
+1. Mobile Navigation
+2. Accordion
+3. Gallery Lightbox
+*/
+
+
+
+/*=============== MOBILE NAVIGATION ============================+*/
+
+
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
   var x = document.getElementById("myTopnav");
@@ -24,4 +35,47 @@ for (i = 0; i < acc.length; i++) {
       panel.style.maxHeight = panel.scrollHeight + "px";
     }
   });
+}
+
+
+/*=============== GALLERY LIGHTBOX============================+*/
+// Open the Modal
+function openModal() {
+  document.getElementById("artmyModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("artmyModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("artmySlides");
+  var dots = document.getElementsByClassName("artalt");
+  var captionText = document.getElementById("artcaption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
