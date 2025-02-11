2a6938ddab2b1f79ec1109cc5b16270916bc6b4f
amizuka000 <62437288+amizuka000@users.noreply.github.com>
2025-02-11 05:20:04 -0500

    Updated index, explore portfolio animation in progress
    Updated content of index. Explore portfolio button in progress, with svg animation.

---
+++ js/index.js
@@ -0,0 +1,8 @@
+
+var path = document.querySelector('.drawArrowLine');
+var length = path.getTotalLength();
+console.log("Line",length);
+
+var arrowHeadPath = document.querySelector('.drawArrowHead');
+var arrowHeadLength = arrowHeadPath.getTotalLength();
+console.log("Arrowhead",arrowHeadLength);
