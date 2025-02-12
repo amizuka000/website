426765f35b5fdac95f01286f86201230f77f452a
amizuka000 <62437288+amizuka000@users.noreply.github.com>
2025-02-12 01:06:09 -0500

    Vertically centered art modal
    Vertically centered art modal

--- js/artgallery.js
+++ js/artgallery.js
@@ -1,6 +1,9 @@
 
 //Load this js file for the art.html
 
+
+
+
 /*=============== GALLERY LIGHTBOX============================+*/
 // Open the Modal
 function openModal() {
@@ -9,8 +12,6 @@ function openModal() {
   //document.getElementById("scrollButton").style.display = "none";
 
   document.body.style.overflow = 'hidden';
-
-
 }
 
 // Close the Modal
@@ -21,6 +22,8 @@ function closeModal() {
   document.body.style.overflow = 'auto';
 
 }
+
+
 
 
 var slideIndex = 1;
@@ -49,7 +52,8 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[slideIndex-1].style.display = "flex";
   dots[slideIndex-1].className += " active";
   //captionText.innerHTML = dots[slideIndex-1].alt;
+
 }
