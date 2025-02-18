67494882b52c8311a0ce534f62d08aa438aff8f2
amizuka000 <62437288+amizuka000@users.noreply.github.com>
2025-02-18 05:16:05 -0500

    Added alts for projects image
    Added alts for projects image, in-progress arrow for mobile index

--- js/index.js
+++ js/index.js
@@ -8,36 +8,47 @@ var arrowHeadPath = document.getElementById("drawArrowHead");
 var arrowHeadLength = arrowHeadPath.getTotalLength();
 console.log("Arrowhead",arrowHeadLength);
 
+/* IN PROGRESS
+var pathHeadHorizontal = document.getElementById("drawArrowHeadHorizontal");
+var lengthHeadHorizontal = pathHeadHorizontal.getTotalLength();
+console.log("HorizontalHead",lengthHeadHorizontal);
+
+var pathLineHorizontal = document.getElementById("drawArrowLineHorizontal");
+var lengthLineHorizontal = pathLineHorizontal.getTotalLength();
+console.log("HorizontalLine",lengthLineHorizontal);
+*/
 
 
-//Click "Explore my portfolio!" to show the arrows
+//Hover "Explore my portfolio!" to show the arrows
 function showArrow(){
   document.getElementById("drawArrowLeft").style.display = "block";
   document.getElementById("drawArrowRight").style.display = "block";
-
-
 }
 
-//Hides the arrows. Initially used when explore button was a hover.
+//Hides the arrows after hover
 function hideArrow(){
   document.getElementById("drawArrowLeft").style.display = "none";
   document.getElementById("drawArrowRight").style.display = "none";
 }
 
+//Shows the arrows for mobile "Explore my portfolio!"
+/*
+function showArrowHorizontal(){
+  document.getElementById("drawArrowHorizontal").style.display = "block";
+}
+*/
 
 //Sets the arrows at a height within the nav bar
 function setArrowHeight() {
-            var headerHeight = document.getElementById('header').clientHeight;
-            var navHeight = document.getElementById('myTopnav').clientHeight;
+  var headerHeight = document.getElementById('header').clientHeight;
+  var navHeight = document.getElementById('myTopnav').clientHeight;
 
-            document.getElementById("drawArrowLeft").style.top = `calc(${headerHeight}px + (${navHeight}px / 4))`;
-            document.getElementById("drawArrowRight").style.top = `calc(${headerHeight}px + (${navHeight}px / 4))`;
-    }
+  document.getElementById("drawArrowLeft").style.top = `calc(${headerHeight}px + (${navHeight}px / 4))`;
+  document.getElementById("drawArrowRight").style.top = `calc(${headerHeight}px + (${navHeight}px / 4))`;
+  }
 
-    window.addEventListener('load', setArrowHeight, true);
-    window.addEventListener('resize', setArrowHeight, true);
-
-
+  window.addEventListener('load', setArrowHeight, true);
+  window.addEventListener('resize', setArrowHeight, true);
 
 
 //Positioning the arrows right outside the navigation links
@@ -54,12 +65,30 @@ function setArrowPosition(){
 
   document.getElementById("drawArrowLeft").style.left = `calc((${totalNavWidth}px - ${navWidth}px)/2 - 60px)`;
   document.getElementById("drawArrowRight").style.right = `calc((${totalNavWidth}px - ${navWidth}px)/2 - 60px)`;
+  }
+
+  window.addEventListener('load', setArrowPosition, true);
+  window.addEventListener('resize', setArrowPosition, true);
 
 
-  console.log(navWidth); // This will print the total width of all the divs
-  console.log(totalNavWidth);
+/*
+function setHorizontalArrowHeight(){
+  var svgHorizontalArrow = document.getElementById('svgHorizontalArrow');
+  var navHeight = document.getElementById('myTopnav').clientHeight;
+  console.log(navHeight);
+  svgHorizontalArrow.setAttribute('height', navHeight);
 
-}
 
-window.addEventListener('load', setArrowPosition, true);
-window.addEventListener('resize', setArrowPosition, true);
+  var headerHeight = document.getElementById('header').clientHeight;
+  document.getElementById("drawArrowHorizontal").style.top = `calc(${headerHeight}px + 10px)`;
+
+  var innerWidth = window.innerWidth;
+  let em = parseFloat(getComputedStyle(document.getElementById('myTopnav')).fontSize); // Adding 1em space (based on font size)
+
+  document.getElementById("drawArrowHorizontal").style.left = `calc(${innerWidth}px - 7*${em}px)`;
+
+    }
+
+  window.addEventListener('load', setHorizontalArrowHeight, true);
+  window.addEventListener('resize', setHorizontalArrowHeight, true);
+*/
