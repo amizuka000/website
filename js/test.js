8b235954e9d16b9dc12bca248706a66555a642c8
amizuka000 <62437288+amizuka000@users.noreply.github.com>
2025-02-24 01:43:55 -0500

    Updated sidenav test
    Updated sidenav test

--- js/test.js
+++ js/test.js
@@ -1,7 +1,10 @@
-function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-}
-
-function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+function openCloseNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+    document.body.style.overflow = 'hidden';
+  } else {
+    x.className = "topnav";
+    document.body.style.overflow = 'auto';
+  };
 }
